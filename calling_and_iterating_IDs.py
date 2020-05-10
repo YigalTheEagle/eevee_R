7358f03962da60682b26261188b82b5126f116ee
YigalTheEagle <33592473+YigalTheEagle@users.noreply.github.com>
2020-05-10 20:27:41 +0300

    New build functionality

--- calling_and_iterating_IDs.py
+++ calling_and_iterating_IDs.py
@@ -6,19 +6,21 @@ from pprint import pprint
 
 import nexmo
 
-client = nexmo.Client(application_id='PUT APPLICATION ID HERE',
-                      private_key='PUT PRIVATE KEY DIRECTORY HERE', )
+client = nexmo.Client(application_id='',
+                      private_key='private.key', )
 
 
 def parse_args():
     parser = argparse.ArgumentParser(epilog='Example: python ' + sys.argv[
         0] + ' -p 1672456824 -t enum -a http://api_server.com/ -enumidstart 555555555 -enumidend 666666666')
+    parser = argparse.ArgumentParser(epilog='Example: python ' + sys.argv[
+        0] + ' -p 1672456824 -t build -a http://api_server.com/ -strings w5,i1,w5,i012345678,r5 --- equivalent of "Wait 5 seconds","Input 1","Wait 5 seconds","Input 012345678","Record 5 seconds"')
     parser.add_argument('-phone', "-p", help="target IVR phone number")
-    parser.add_argument('-t', help="Choose the test type. id enumeration, dos versus a supplied id, or strings",
-                        choices=['enum', 'dos', 'strings'], default='id', nargs='?')
+    parser.add_argument('-t', '-type' ,help="Choose the test type. id enumeration, dos versus a supplied id, or strings",
+                        choices=['enum', 'dos', 'strings','record','build'], default='id', nargs='?')
     parser.add_argument('-a', "-api", help="your API server address, to communicate with nexmo's API server")
-    parser.add_argument("-enumidstart", help="the ID to start from")
-    parser.add_argument("-enumidend", help="the last ID in the range")
+    parser.add_argument("-enumidstart", '-idstart' ,help="the ID to start from")
+    parser.add_argument("-enumidend", '-idend',help="the last ID in the range")
     parser.add_argument("-dospins",
                         help="the pins to send in the conversation, seperated by a comma, for example 123456,123154,123123")
     parser.add_argument("-strings", help="The DTMF strings to send in the conversation, for example abcdpp*123")
@@ -47,24 +49,38 @@ def iterate_on_ids(ncco, start_id, end_id):
     for x in range(int(start_id), int(end_id)):
         call_this(ncco, x)
 
+def call_this_only_record(ncco):
+    calledncco = deepcopy(ncco) #had to use deepcopy to copy the type of ncco (dict list)
+    calledncco["answer_url"] = [calledncco["answer_url"][0]]
+    response = client.create_call(calledncco)
+    pprint("sending call for record testing")
+
 
 def main():
     chosenArgs = parse_args()
     ncco = build_first_ncco(chosenArgs.phone, chosenArgs.a)
     if (chosenArgs.t == "enum"):
         pprint('You chose ID enumeration bruteforcing by range - enumidstart is the range start, enumidend is the end')
-        ncco["answer_url"] = [ncco["answer_url"][0] + "answering/"]
+        ncco["answer_url"] = [ncco["answer_url"][0] + "api/answering/"]
         iterate_on_ids(ncco, int(chosenArgs.enumidstart), int(chosenArgs.enumidend))
         pprint("Finished calling enumeration on all provided IDs")
     if (chosenArgs.t == "dos"):
         pprint("You chose DOSing IDs with PINs seperated by a comma")
-        ncco["answer_url"] = [ncco["answer_url"][0] + "answering-dos/" + str(chosenArgs.dospins)+"/"]
+        ncco["answer_url"] = [ncco["answer_url"][0] + "api/answering-dos/" + str(chosenArgs.dospins)+"/"]
         iterate_on_ids(ncco, int(chosenArgs.enumidstart), int(chosenArgs.enumidend))
         pprint("Finished sending PINs for all IDs")
     if (chosenArgs.t == "strings"):
         pprint("You chose sending strings to enumerate admin interfaces and cause weird behaviors")
-        ncco["answer_url"] = [ncco["answer_url"][0] + "answering_strings/"+str(chosenArgs.strings)]
-        call_creation_strings(urllib.parse.quote(chosenArgs.strings))
+        ncco["answer_url"] = [ncco["answer_url"][0] + "api/answering_strings/"+str(chosenArgs.strings)]
+        call_creation_strings(ncco,urllib.parse.quote(chosenArgs.strings))
+    if (chosenArgs.t == "record"):
+        pprint("You chose recording the conversation for testing")
+        ncco["answer_url"] = [ncco["answer_url"][0] + "api/testrecording/"]
+        call_this_only_record(ncco)
+    if (chosenArgs.t == "build"):
+        pprint("You chose building an ncco from scratch")
+        ncco["answer_url"] = [ncco["answer_url"][0] + "api/buildncco/"+str(urllib.parse.quote(chosenArgs.strings))]
+        client.create_call(ncco)
 
 
 if __name__ == "__main__":
--- event_api.py
+++ event_api.py
@@ -5,123 +5,263 @@ from pprint import pprint
 import librosa
 from dtw import dtw
 from numpy.linalg import norm
+import urllib.parse
 import os
+from requests import get
+import time
 
-client = nexmo.Client(application_id='PUT APPLICATION ID HERE',private_key='PUT PRIVATE KEY DIRECTORY HERE',) # make sure to place your nexmo appID and private key here
+
+
+#Implement another 'class' file that will contain all the API endpoint paths, and the application_id and private key of the application
+
+
+
+
+#
+
+
+client = nexmo.Client(application_id='',private_key='private.key',) # make sure to place your nexmo appID and private key file here
 app=Flask(__name__)
 ConvoArr={}
+
+
+
+API_endpoint = 'http://'+get('https://api.ipify.org').text
 
 def load_files_for_audio_comp(file1):
     y1, sr1 = librosa.load(file1) #change this to the location of the audiofile with the "wrong ID" recording
     return y1,sr1
 
+#This function returns the distance between the signals in the two provided mp3 files. The higher the value, the more different are the signals in the recordings.
 def compare_Audio_Files(file1,file2):
     y1,sr1=load_files_for_audio_comp(file1)
     y2,sr2=load_files_for_audio_comp(file2)
     mfcc1 = librosa.feature.mfcc(y1,sr1)
     mfcc2 = librosa.feature.mfcc(y2, sr2)
-    idontknow=lambda x,y:norm(x-y, ord=1)
-    dist, cost, path ,what= dtw(mfcc1.T, mfcc2.T,dist=idontknow)
+    distance = lambda x, y: norm(x-y, ord = 1)
+    dist, cost, path, what= dtw(mfcc1.T, mfcc2.T,dist=distance)
     return dist
 
+#As the name implies, this function receives a conversation UUID (received from the answering API endpoints), and sends through DTMF the targeted ID number 
 def sending_id_for_convo_uuid(convo_uuid):
     for single_digit in ConvoArr[convo_uuid]['personal_id']:
-        whatever = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
-    return 'boom boom'
+        client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
 
+#As the name implies, this function receives a conversation UUID and sends through DTMF PINS
 def sending_pin_for_convo_uuid(convo_uuid,pin):
     for single_digit in pin:
-        whatever = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
-    return 'boom boom'
+        client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_digit)
 
 def sending_strings_for_convo_uuid(convo_uuid):
     for x in ConvoArr[convo_uuid]['strings_to_send']:
-        whateverr = client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=x)
-    return 'pow'
+        client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=x)
 
-@app.route("/api/answering/<string:personal_id>")
-def answer_call(personal_id):
-    while(len(str(personal_id))<9):
-        personal_id="0"+personal_id
-    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id}
-    ncco = [{"action": "input", "eventUrl": ["http://api_endpoint/api/send_id"], "name": "waiting", "timeOut": 3},{"action": "record","eventUrl": ["http://api_endpoint/api/recordings/id"], "name": "send_record","timeOut":4}]
-    print("Started call for ID: "+personal_id)
-    return jsonify(ncco)
+def send_dtmf_for_convo_uuid(convo_uuid,dtmf):
+    for single_item in dtmf:
+        client.send_dtmf(ConvoArr[convo_uuid]['uuid'], digits=single_item)
 
-@app.route("/api/answering-dos/<string:pins>/<string:personal_id>/")
-def answer_all_with_dos(personal_id,pins):
-    pinArray=pins.split(",")
-    ncco=[{"action":"input","eventUrl":["http://api_endpoint/api/send_id"],"name":"waiting","timeOut":3},{"action":"record","eventUrl":["http://api_endpoint/api/recordings/dos"],"name":"send_record"}]
-    for i in range(0,len(pinArray)):
-        ncco.append({"action": "input", "eventUrl": ["http://api_endpoint/api/send_pin/" + pinArray[i]], "name":"waiting","timeOut": 5})
-    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id,"pinArray":pins}
-    ncco.append({"action":"input","eventUrl":["http://api_endpoint/api/just_wait/"],"name":"waiting","timeOut":15})
-    print("Started DOS call for ID: " + personal_id+", with PINs "+pins)
-    return jsonify(ncco)
+def ncco_create_talk_action(whattosay):
+    speech = whattosay.replace("_"," ")
+    return {"action":"talk","text":speech}
 
-@app.route("/api/answering_strings/<string:strings_to_send>")
-def sending_abcd(strings_to_send):
-    ConvoArr[request.args.get('conversation_uuid')] = {"uuid": request.args.get('uuid'), "strings_to_send": strings_to_send}
-    timeoutseconds=len(strings_to_send)/3
-    ncco = [
-        {"action": "input", "eventUrl": ["http://api_endpoint/api/send_strings"], "name": "waiting", "timeOut": timeoutseconds},
-        {"action": "record", "eventUrl": ["http://api_endpoint/api/recordings/strings"], "name": "send_record",
-         "timeOut": 10}]
-    return jsonify(ncco)
+def ncco_create_record_action_for_length(thelength):
+    #Currently, the eventUrl is not dynamic, might need to edit this -- optionally add additional parsing for recording options, for instance comparing with wrong or right FOR EXAMPLE: w5,544363125,r5cwr,w5,1332 (wait 5, input id, record for 5 seconds and compare against wrong + right (cwr), wait 5, input 1332)
+    #Additional functionality could be to add if's inside the parameters, or all characters from client side on a single digit, launching conversations to all combinations, comparing their results with one another
+    #implement something in the api_endpoint eventurl
+    return {"action":"record","eventUrl":[API_endpoint+"/api/recordings/test"],"name":"RecordFor"+thelength,"timeOut":thelength}
+
+def ncco_create_wait_action(ttl):
+    #possibly add the ability to create a custom eventUrl
+    return {"action":"input","eventUrl": [API_endpoint+"/api/just_waiting"], "name": "waiting", "timeOut":ttl}
+
+def ncco_delimit_string_until_comma(thestring):
+    return thestring[0:thestring.index(',')-1]
+
+def ncco_create_this_input(whattosend):
+    #possibly add the ability to create a custom eventUrl
+    return {"action":"input","eventUrl": [API_endpoint+"/api/send_this/"+urllib.parse.quote(whattosend)], "name": "inserting_this", "timeOut":len(whattosend)/3}
+
+#build parameter should be along the lines of i5,544363125,r5,tthank_you, -- wait 5 seconds, input series of number, record 5 seconds, say thank you
+def build_ncco(parameters):
+    ncco = []
+    loops = parameters.split(",")
+    for i in loops:
+        if(i[0]=='t' or i[0]=='T'):
+            ncco.append(ncco_create_talk_action(i[1:]))
+        if(i[0]=='r' or i[0]=='R'):
+            ncco.append(ncco_create_record_action_for_length(i[1:]))
+        if(i[0]=='w' or i[0]=='W'):
+            ncco.append(ncco_create_wait_action(i[1:]))
+        if(i[0]=='i' or i[0]=='I'):
+            ncco.append(ncco_create_this_input(i[1:]))
+    return ncco
+        
+# R + Num of seconds to record (delimiter is ,)
+# : will be a brief pause
+# t will be talk, followed by text to speak (delimiter is ,)
+
+#
+# ncco = [
+#         {"action": "record", "eventUrl": [API_endpoint+"/api/recordings/strings"], "name": "send_record","timeOut": 10},
+#         {"action": "input", "eventUrl": [API_endpoint+"/api/send_strings"], "name": "waiting", "timeOut": timeoutseconds},
+#         ]
+#=================================== API Routes to send data mid conversation ======================================== 
 
 @app.route("/api/send_id", methods=['POST'])
 def sending_id():
     data = request.get_json()
     sending_id_for_convo_uuid(data['conversation_uuid'])
-    return "I wish I was a little bit taller"
-
+    return "Sending"
 
 @app.route("/api/send_strings", methods=['POST'])
 def sending_strings():
     data = request.get_json()
     sending_strings_for_convo_uuid(data['conversation_uuid'])
-    return "I wish I was a baller"
+    return "Sending"
 
 @app.route("/api/send_pin/<string:pin>", methods=['POST'])
 def sending_pin(pin):
     data = request.get_json()
     sending_pin_for_convo_uuid(data['conversation_uuid'],pin)
-    return "I wish I had a girl, that looked good, I would call her"
+    return "Sending"
+
+@app.route("/api/send_this/<string:whattosend>",methods=['POST'])
+def sending_this(whattosend):
+    data = request.get_json()
+    send_dtmf_for_convo_uuid(data['conversation_uuid'],whattosend)
+    return "Sending"
+
+
+#=================================== Recording Functionality API route ==============================
 
 @app.route("/api/recordings/<string:functionality>", methods=['POST'])
 def recordings(functionality):
     data=request.get_json()
     a_record=client.get_recording(data['recording_url'])
     if(functionality=='id'):
-        fileName="ID_"+ConvoArr[data['conversation_uuid']]['personal_id']+".mp3"
-        f=open(fileName,'wb+')
-        f.write(a_record)
-        f.close()
+        functionalityid(data,a_record)
+    if (functionality == 'dos'):
+        functionalitydos(data,a_record)
+    if(functionality=='strings'):
+        functionalitystrings(data,a_record)
+    if(functionality=='test'):
+        functionalitytestrecording(data,a_record)
+        
+    return "" #can send an ncco object to continue the conversation
+
+#=================================== Recording Functionality Helpers ==============================
+
+def functionalityid(data,a_record):
+    fileName="ID_"+ConvoArr[data['conversation_uuid']]['personal_id']+".mp3"
+    f=open(fileName,'wb+')
+    f.write(a_record)
+    f.close()
+    try:
         comparedValue=compare_Audio_Files('wrongID.mp3',fileName)
-        if(int(comparedValue)>40000 and int(comparedValue)<150000): # this will check if the file is different from the wrong ID recording, above 40000 value means the recording contains a different phrase WARNING: will contain false positives due to inconsistency in the phone line
+        if(int(comparedValue) > 40000 and int(comparedValue) < 150000): # this will check if the file is different from the wrong ID recording, above 40000 value means the recording contains a different phrase WARNING: will contain false positives due to inconsistency in the phone line
             new_file_name = "Difference_" + str(int(comparedValue)) + "_" + fileName
             print("The ID: "+ConvoArr[data['conversation_uuid']]['personal_id']+" returned a different response, comparing it against the right ID recording")
-            newcomparison=compare_Audio_Files('rightID.mp3',fileName)
-            if(int(newcomparison)<45000):
-                new_file_name = "RightID_Difference_" + str(int(newcomparison)) + "_"+fileName
-                os.rename(fileName,new_file_name)
-                print("The ID " +ConvoArr[data['conversation_uuid']]['personal_id'] + " sounds like the right ID recording!!!!")
-            else:
-                os.rename(fileName, new_file_name)
+            try:
+                newcomparison=compare_Audio_Files('rightID.mp3',fileName)
+                if(int(newcomparison) < 45000):
+                    new_file_name = "RightID_Difference_" + str(int(newcomparison)) + "_"+fileName
+                    os.rename(fileName,new_file_name)
+                    print("The ID " +ConvoArr[data['conversation_uuid']]['personal_id'] + " sounds like the right ID recording!!!!")
+                else:
+                    os.rename(fileName, new_file_name)
+            except:
+                print('No rightID recording present')
         else:
             os.rename(fileName, "maybe_garbage" + fileName)
-    if (functionality == 'dos'):
-        fileName = "ID_DOSed_" + ConvoArr[data['conversation_uuid']]['personal_id'] + "_PINS_"+str(ConvoArr[data['conversation_uuid']]["pinArray"])+".mp3"
-        f = open(fileName, 'wb+')
-        f.write(a_record)
-        f.close()
-        print("ID: " + str(ConvoArr[data['conversation_uuid']]['personal_id']) + " was DOSed with PINs: " + str(ConvoArr[data['conversation_uuid']]["pinArray"]))
-    if(functionality=='strings'):
-        f = open("Sent_Strings_" + ConvoArr[data['conversation_uuid']]['strings_to_send'] + ".mp3", 'wb+')
-        f.write(a_record)
-        f.close()
-    return "I wish I had a girl, that looked good, I would call her"
+    except:
+        print('No WrongID recording present, please listen to current recording to determine if it suits')
 
+def functionalitydos(data,a_record):
+    fileName = "DOS_Num_"+ConvoArr[data['conversation_uuid']]["PhoneNumber"]+"_ID_" + ConvoArr[data['conversation_uuid']]['personal_id'] + "_PINS_"+str(ConvoArr[data['conversation_uuid']]["pinArray"])+".mp3"
+    f = open(fileName, 'wb+')
+    f.write(a_record)
+    f.close()
+    print("ID: " + str(ConvoArr[data['conversation_uuid']]['personal_id']) + " was DOSed with PINs: " + str(ConvoArr[data['conversation_uuid']]["pinArray"]))
+
+def functionalitystrings(data,a_record):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    f = open("Sent_Strings_To_"+ConvoArr[data['conversation_uuid']]["PhoneNumber"]+"Strings_" + ConvoArr[data['conversation_uuid']]['strings_to_send'] + "_"+ timestr +".mp3", 'wb+')
+    f.write(a_record)
+    f.close()
+
+def functionalitytestrecording(data,a_record):
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    f = open("test-recording"+ConvoArr[data['conversation_uuid']]["PhoneNumber"]+"_At_"+timestr+".mp3", 'wb+')
+    f.write(a_record)
+    f.close()
+
+    # ============================= call answer functionality API routes =========================
+
+@app.route("/api/testrecording/")
+def record_this_call():
+    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":None,"PhoneNumber":request.args.get("to")}
+    ncco = [
+        {"action": "record","eventUrl": [API_endpoint+"/api/recordings/test"], "name": "send_record","timeOut":10}
+        ]
+    print("Started call for recording")
+    return jsonify(ncco)
+
+
+@app.route("/api/answering/<string:personal_id>")
+def answer_call(personal_id):
+    while(len(str(personal_id))<9):
+        personal_id="0"+personal_id
+    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id,"PhoneNumber":request.args.get("to")}
+    ncco = [
+        {"action": "input", "eventUrl": [API_endpoint+"/api/send_id"], "name": "waiting", "timeOut": 3},
+        {"action": "record","eventUrl": [API_endpoint+"/api/recordings/id"], "name": "send_record","timeOut":4}
+        ]
+    print("Started call for ID: "+personal_id)
+    return jsonify(ncco)
+
+#============ This is the dynamically Built NCCO ===========
+
+@app.route("/api/buildncco/<string:conversationdata>")
+def create_the_ncco_call(conversationdata):
+    pprint(conversationdata)
+    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"PhoneNumber":request.args.get("to")}
+    ncco = build_ncco(conversationdata)
+    pprint(ncco)
+    print("Started call for build NCCO: "+conversationdata)
+    return jsonify(ncco)
+
+#===========================================================
+
+@app.route("/api/answering-dos/<string:pins>/<string:personal_id>/")
+def answer_all_with_dos(personal_id,pins):
+    pinArray=pins.split(",")
+    ncco=[
+        {"action":"input","eventUrl":[API_endpoint+"/api/send_id"],"name":"waiting","timeOut":3},
+        {"action":"record","eventUrl":[API_endpoint+"/api/recordings/dos"],"name":"send_record"}
+        ]
+    for i in range(0,len(pinArray)):
+        ncco.append({"action": "input", "eventUrl": [API_endpoint+"/api/send_pin/" + pinArray[i]],"name":"waiting","timeOut": 5})
+                    
+    ConvoArr[request.args.get('conversation_uuid')]={"uuid":request.args.get('uuid'),"personal_id":personal_id,"pinArray":pins,"PhoneNumber":request.args.get("to")}
+    ncco.append({"action":"input","eventUrl":[API_endpoint+"/api/just_wait/"],"name":"waiting","timeOut":15})
+    
+    print("Started DOS call for ID: " + personal_id+", with PINs "+pins)
+    return jsonify(ncco)
+
+
+@app.route("/api/answering_strings/<string:strings_to_send>")
+def sending_abcd(strings_to_send):
+    
+    ConvoArr[request.args.get('conversation_uuid')] = {"uuid": request.args.get('uuid'), "strings_to_send": strings_to_send,"PhoneNumber":request.args.get("to")}
+    timeoutseconds=len(strings_to_send)/3
+    ncco = [
+        {"action": "record", "eventUrl": [API_endpoint+"/api/recordings/strings"], "name": "send_record","timeOut": 10},
+        {"action": "input", "eventUrl": [API_endpoint+"/api/send_strings"], "name": "waiting", "timeOut": timeoutseconds},
+        {"action": "record", "eventUrl": [API_endpoint+"/api/recordings/strings"], "name": "send_record","timeOut": 10}
+        ]
+    return jsonify(ncco)
+
+    # ============================= Main =====================================
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
